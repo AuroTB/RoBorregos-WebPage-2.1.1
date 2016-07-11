73efe9dcb5db32dd64b83f1d73bf3739e2508aad
Eduardo Sanchez <gallo.sanchez23@gmail.com>
2016-07-11 00:38:28 -0500

    Add a header and some parallax scrolling stuff. Fixed some sponsors-sidebar issues.

---
+++ js/functions.js
@@ -0,0 +1,53 @@
+$(window).scroll(function(){
+
+	var wScroll = $(this).scrollTop();
+
+	$('.logo-header').css({
+		'transform' : 'translate(0px, '+ wScroll /2 +'px)'
+	});
+
+	$('.front-robot').css({
+		'transform' : 'translate(0px, '+ wScroll /15 +'%)'
+	});
+
+	$('.back-robot').css({
+		'transform' : 'translate(0px, '+ wScroll /7 +'%)'
+	});
+
+	console.log(wScroll)
+
+});
+
+(function(){
+
+			var bodyEl = $('body'),
+				navToggle = bodyEl.find('.side-navbar'),
+				navToggleSponsors = bodyEl.find('.side-navbar-sponsors'),
+				btnSide1 = bodyEl.find('.btn-side-navbar-1');
+
+			navToggle.on('mouseover', function(e){
+				bodyEl.toggleClass('active-nav');
+				e.preventDefault();
+			});
+
+			navToggle.on('mouseout', function(e){
+				bodyEl.toggleClass('active-nav');
+				e.preventDefault();
+			});
+
+			navToggleSponsors.on('mouseover', function(e){
+				bodyEl.toggleClass('active-nav-sponsors');
+				e.preventDefault();
+			});
+
+			navToggleSponsors.on('mouseout', function(e){
+				bodyEl.toggleClass('active-nav-sponsors');
+				e.preventDefault();
+			});
+
+			btnSide1.on('click', function(e){
+				bodyEl.toggleClass('rotate-arrow-1');
+				e.preventDefault();
+			});
+
+		})();
