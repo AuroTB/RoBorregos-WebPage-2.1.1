06cd435c114095980afeb1ac8bb47d63b2d712b0
Eduardo Sanchez <gallo.sanchez23@gmail.com>
2016-07-12 02:12:33 -0500

    cleaned code on index.html, style.css and functions.js files

--- js/functions.js
+++ js/functions.js
@@ -20,34 +20,34 @@ $(window).scroll(function(){
 
 (function(){
 
-			var bodyEl = $('body'),
-				navToggle = bodyEl.find('.side-navbar'),
-				navToggleSponsors = bodyEl.find('.side-navbar-sponsors'),
-				btnSide1 = bodyEl.find('.btn-side-navbar-1');
+	var bodyEl = $('body'),
+	navToggle = bodyEl.find('.side-navbar'),
+	navToggleSponsors = bodyEl.find('.side-navbar-sponsors'),
+	btnSide1 = bodyEl.find('.btn-side-navbar-1');
 
-			navToggle.on('mouseover', function(e){
-				bodyEl.toggleClass('active-nav');
-				e.preventDefault();
-			});
+	navToggle.on('mouseover', function(e){
+		bodyEl.toggleClass('active-nav');
+		e.preventDefault();
+	});
 
-			navToggle.on('mouseout', function(e){
-				bodyEl.toggleClass('active-nav');
-				e.preventDefault();
-			});
+	navToggle.on('mouseout', function(e){
+		bodyEl.toggleClass('active-nav');
+		e.preventDefault();
+	});
 
-			navToggleSponsors.on('mouseover', function(e){
-				bodyEl.toggleClass('active-nav-sponsors');
-				e.preventDefault();
-			});
+	navToggleSponsors.on('mouseover', function(e){
+		bodyEl.toggleClass('active-nav-sponsors');
+		e.preventDefault();
+	});
 
-			navToggleSponsors.on('mouseout', function(e){
-				bodyEl.toggleClass('active-nav-sponsors');
-				e.preventDefault();
-			});
+	navToggleSponsors.on('mouseout', function(e){
+		bodyEl.toggleClass('active-nav-sponsors');
+		e.preventDefault();
+	});
 
-			btnSide1.on('click', function(e){
-				bodyEl.toggleClass('rotate-arrow-1');
-				e.preventDefault();
-			});
+	btnSide1.on('click', function(e){
+		bodyEl.toggleClass('rotate-arrow-1');
+		e.preventDefault();
+	});
 
-		})();
+})();
